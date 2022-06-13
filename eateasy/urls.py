dd991dfa68c04996b43046b60cb801d3fe9a7f94
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-06-13 12:09:39 +0000

    add recipe detail view, template and wire up urls

--- eateasy/urls.py
+++ eateasy/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.RecipeList.as_view(), name='home')
+    path('', views.RecipeList.as_view(), name='home'),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
 ]
--- eateasy/views.py
+++ eateasy/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -8,3 +8,22 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        # favourite = False
+        # if recipe.favourites.filter
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                # "favourite": favourite
+            }
+        )
