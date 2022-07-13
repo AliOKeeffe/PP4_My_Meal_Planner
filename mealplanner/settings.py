c54134e83309467423b2261574b20396b54a4abc
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-07-13 09:31:21 +0000

    fix static file url for push to heroku

--- mealplanner/settings.py
+++ mealplanner/settings.py
@@ -164,7 +164,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
