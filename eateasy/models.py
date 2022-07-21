ad2e3995ef0f9bc88b1811f5007d697cd10898da
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-07-21 15:46:51 +0000

    Change length of recipe title in model and fix responsiveness of recipe detail page

--- eateasy/models.py
+++ eateasy/models.py
@@ -11,7 +11,7 @@ STATUS = ((0, "Save for later"), (1, "Publish Now"))
 
 class Recipe(models.Model):
     """Model for Recipe"""
-    title = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=50, unique=True)
     slug = AutoSlugField(populate_from='title', unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="recipes")
