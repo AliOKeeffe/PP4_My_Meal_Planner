8cacf35dfb7daf371cebc3adb376a5f9db073266
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-08-02 11:00:39 +0000

    Add link to slack conversation to credit Ian Meigh for form validator

--- eateasy/validators.py
+++ eateasy/validators.py
@@ -8,7 +8,9 @@ def textfield_not_empty(textfield):
     """
     Strip white space from textfield and raise validation error
     if field is left empty
-    Credit: Ian Meigh_5P for his help with this validator on stack overflow.
+    Credit: Ian Meigh_5P for his help with this validator on the code
+    Institute's slack channel:
+    https://code-institute-room.slack.com/archives/CGWQJQKC5/p1659026298076079?thread_ts=1659005118.161939&cid=CGWQJQKC5.
     """
     cleaned_data = strip_tags(textfield).replace("&nbsp;", "").strip()
     if cleaned_data == "":
